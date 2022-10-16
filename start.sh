087ef0198de096e6c7a56c4d041ae12d6445397a
chun-awa <1951474558@qq.com>
2022-10-16 20:39:22 +0800

    modified:   start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qemu-system-x86_64 $(test -e /dev/kvm&&echo -enable-kvm) -kernel vmlinuz-6.0 -initrd rootfs.bz2 -m 256 -append "quiet vga=836"
+qemu-system-x86_64 $(test -e /dev/kvm&&echo -enable-kvm) -kernel vmlinuz-*[0] -initrd rootfs.bz2 -m 256 -append "quiet vga=836" -smp 4
