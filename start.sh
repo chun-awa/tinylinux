5989aa31380dccf64ab62f54fb55e780e956b4fd
chun-awa <1951474558@qq.com>
2022-11-27 15:14:21 +0800

    -

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-if test ${DISPLAY};then
-    vga=836
-else
-    vga=3846
-fi
-qemu-system-x86_64 $(test -e /dev/kvm&&echo -enable-kvm) -kernel vmlinuz-*[0] -initrd rootfs.bz2 -m 256 -append "quiet vga=${vga}" -smp 4 $(test ${DISPLAY}||echo -display curses)
+qemu-system-x86_64 $(test -e /dev/kvm&&echo -enable-kvm) -kernel vmlinuz-* -initrd rootfs.bz2 -m 256 -append "quiet" -smp 4
