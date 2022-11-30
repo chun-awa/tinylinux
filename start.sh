013c8181d4ea636766b9c6db0bc093db7e8ad927
chun-awa <1951474558@qq.com>
2022-11-30 17:04:19 +0800

    -

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qemu-system-x86_64 $(test -e /dev/kvm&&echo -enable-kvm) -kernel vmlinuz-* -initrd rootfs.bz2 -m 256 -append "quiet" -smp 4
+qemu-system-x86_64 $([ -e /dev/kvm ]&&echo "-enable-kvm") -kernel vmlinuz-* -initrd rootfs.bz2 -m 256 -append "quiet" -smp 4 $([ "${DISPLAY}" ]||echo "-display curses")
