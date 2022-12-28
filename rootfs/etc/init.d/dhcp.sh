c9fb5ed0ed545d2df694458231cd69ae963306ed
chun-awa <1951474558@qq.com>
2022-12-28 19:41:12 +0800

    updated dhcp.sh

--- rootfs/etc/init.d/dhcp.sh
+++ rootfs/etc/init.d/dhcp.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+. /etc/init.d/rc.lib
 ip addr add $ip/$mask dev $interface
 [ "$router" ] && ip route add default via $router dev $interface
+if [ "$ip" ];then
+    log_info "DHCP configuration for device ${interface}"
+    log_info "IP: ${ip}"
+    log_info "mask: ${mask}"
+    log_info "router: ${router}"
+fi
