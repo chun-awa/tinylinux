05c792f28e28ce0c079ed8b44f3970167514480d
chun-awa <1951474558@qq.com>
2023-01-04 14:29:34 +0800

    updated start,sh

--- start.sh
+++ start.sh
@@ -6,47 +6,47 @@ fi
 
 kernel=vmlinuz-4.9.330
 initrd=rootfs.cpio
-if [ -e /dev/kvm ];then
-	kvm="-enable-kvm"
-else
-	kvm=
-fi
-if [ "${1}" = "tcg" ];then
-	kvm=  
-fi
-if [ "${DISPLAY}" ];then
-	vga=0x344
-else
-	lines=$(tput lines)
-	if [ ${lines} -gt 60 ];then
-		vga=0xf07
-	elif [ ${lines} -gt 50 ];then
-		vga=0xf01
-	elif [ ${lines} -gt 43 ];then
-		vga=0xf02
-	elif [ ${lines} -gt 34 ];then
-		vga=0xf06
-	elif [ ${lines} -gt 30 ];then
-		vga=0xf05
-	elif [ ${lines} -gt 28 ];then
-		vga=0xf03
-	else
-		vga=0xf00
-    fi
-fi
+[ -e /dev/kvm ]&&kvm="-enable-kvm"
+vga=0x344
+for i in "${@}";do
+    case "${i}" in
+    	tcg)
+    		kvm=
+    		;;
+		curses)
+			display="-display curses"
+			lines=$(tput lines)
+			if [ ${lines} -gt 60 ];then
+			    vga=0xf07
+			elif [ ${lines} -gt 50 ];then
+			    vga=0xf01
+			elif [ ${lines} -gt 43 ];then
+			    vga=0xf02
+			elif [ ${lines} -gt 34 ];then
+			    vga=0xf06
+			elif [ ${lines} -gt 30 ];then
+			    vga=0xf05
+			elif [ ${lines} -gt 28 ];then
+			    vga=0xf03
+			else
+			    vga=0xf00
+			fi
+			;;
+    	*)
+			echo "Unknown option:${i}"
+			exit 1
+			;;
+    esac
+done
 cmdline="quiet vga=${vga}"
 smp=4
 memory=512
-if [ "${DISPLAY}" ];then
-    display="gtk"
-else
-    display="curses"
-fi
-
-qemu-system-x86_64 ${kvm}\
+echo "Starting QEMU..."
+qemu-system-x86_64\
+	${kvm}\
     -kernel "${kernel}"\
     -initrd "${initrd}"\
     -m ${memory}\
     -append "${cmdline}"\
     -smp ${smp}\
-    -display ${display}\
+    "${display}"
